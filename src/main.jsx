f43d1b8d5cbfbfff10246c696791a57ca018b302
morasofia <morasofia51206@gmail.com>
2026-01-14 20:24:43 -0300

    whatsapp

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'//IMPORTANTISIMO PARA QUE FUNCIONE
-import ThemeContextProvider from './Contexts/themeContexts.jsx'
+import ThemeContextProvider from './Contexts/ThemeContexts.jsx'
 createRoot(document.getElementById('root')).render(
     /* Importante para poder usar el router */
     <BrowserRouter>
